85c16d96f59a23eea089d73e57184c0fdb7ba1f2
Silvia Rocha <silviadprocha@gmail.com>
2020-03-23 20:57:24 +0900

    add balance component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function Header() {
-  return <h2>Expense Tracker</h2>;
-}
+export const Header = () => {
+  return (
+    <div>
+      <h2>Expense Tracker</h2>
+    </div>
+  );
+};
